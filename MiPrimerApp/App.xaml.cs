147266ac95bb58940073dfd930d2428336d3288a
Vivian Munguia <andy_munguia@hotmail.com>
2022-05-04 17:16:18 -0700

    A label and an entry were added

--- MiPrimerApp/App.xaml.cs
+++ MiPrimerApp/App.xaml.cs
@@ -10,7 +10,7 @@ namespace MiPrimerApp
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new MyContentPage();
         }
 
         protected override void OnStart()
---
+++ MiPrimerApp/MyContentPage.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace MiPrimerApp
+{
+    public class MyContentPage : ContentPage
+    {
+        public MyContentPage()
+        {
+            var label = new Label
+            {
+                Text = "Escribe tu nombre"
+            };
+            var txtNombre = new Entry
+            {
+                Placeholder = "Escribe tu nombre"
+            };
+            Content = new StackLayout
+            {
+                Padding = 30,
+                Spacing = 10,
+                Children = { label, txtNombre }
+            };
+        }
+    }
+}
+
+
