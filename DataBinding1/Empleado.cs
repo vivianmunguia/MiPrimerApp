718f730f2c2d3bf89c0b4362b7ea99baddbe392d
Vivian Munguía <andy_munguia@hotmail.com>
2022-05-29 20:25:08 -0700

    Project creation that uses databindiing

---
+++ DataBinding1/Empleado.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataBinding1
+{
+    public class Empleado
+    {
+        public string NombreCompleto { get; set; }
+        public string Email { get; set; }
+    }
+}
+
---
+++ DataBinding1/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace DataBinding1
+{
+    public partial class MainPage : ContentPage
+    {
+        public Empleado NuevoEmpleado { get; set; }
+        public MainPage()
+        {
+            NuevoEmpleado = new Empleado();
+            NuevoEmpleado.NombreCompleto = "Vivian Munguía";
+            NuevoEmpleado.Email = "vivian.munguia.mtz@gmail.com";
+            BindingContext = NuevoEmpleado;
+            InitializeComponent();
+        }
+    }
+}
